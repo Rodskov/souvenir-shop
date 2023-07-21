08b369531797d8c62b2d1bf9b7ad10eba654a5f5
Kyle Carandang <92615736+kylekrc@users.noreply.github.com>
2023-07-22 04:46:30 +0800

    Finished 50

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,142 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import styles from "./ViewProducts.module.scss";
+import { toast } from 'react-toastify';
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db, storage } from '../../../firebase/config';
+import { Link } from 'react-router-dom';
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Loader from '../../loader/Loader';
+import { deleteObject, ref } from 'firebase/storage';
+import Notiflix from 'notiflix';
 
 const ViewProducts = () => {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = () => {
+    setIsLoading(true)
+
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+        setIsLoading(false);
+      });
+
+    } catch(error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      'Delete Product?',
+      'You are about to delete this product',
+      'Delete',
+      'Cancel',
+      function okCb() {
+        deleteProduct(id, imageURL)
+      },
+      function cancelCb() {
+        console.log("Delete Cancelled.")
+      },
+      {
+        width: '320px',
+        borderRadius: '8px',
+        titleColor: 'orangered',
+        okButtonBackground: 'orangered',
+        cssAnimationStyle:'zoom'
+        // You can adjust design here
+      },
+    );
+  };
+
+  const deleteProduct = async(id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+
+      const storageRef = ref(storage, imageURL);
+      await deleteObject(storageRef)
+      toast.success("Product deleted successfully.")
+
+    } catch(error) {
+      toast.error(error.message)
+    }
+  }
+
+  return (
+    <>
+    {isLoading && <Loader/>}
+    <div className={styles.table}>
+      <h2>All Products</h2>
+
+      {products.length === 0 ? (
+        <p>No products found.</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>s/n</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          {products.map((product, index) => {
+            const {id, name, price, imageURL, category} = product;
+            return (
+              
+              <tr key={id}>
+                <td>
+                  {index + 1}
+                </td>
+                <td>
+                  <img src={imageURL} alt={name} style={{width: "100px"}}/>
+                </td>
+                <td>
+                  {name}
+                </td>
+                <td>
+                  {category}
+                </td>
+                <td>
+                  {`₱${price}`}
+                </td>
+                <td className={styles.icons}>
+                  <Link to="/admin/add-product">
+                  <FaEdit size={20} color='green'/>
+                  </Link>
+                  &nbsp;
+                  <FaTrashAlt size={18} color='red'
+                  onClick={() => confirmDelete(id, imageURL)}/>
+                </td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+      )}
+    </div>
+      
+    </>
+  );
 };
 
 export default ViewProducts;
