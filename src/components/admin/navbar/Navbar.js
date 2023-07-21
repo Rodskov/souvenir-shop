ba7bfc8c469792158329065722e5a4ebea3c45bd
Kyle Carandang <92615736+kylekrc@users.noreply.github.com>
2023-07-22 03:09:56 +0800

    Finished 45

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import styles from "./Navbar.module.scss";
+import { useSelector } from 'react-redux';
+import { selectUserName } from '../../../redux/slice/authSlice';
+import { NavLink } from 'react-router-dom';
+import { FaUserCircle } from "react-icons/fa";
+
+const activeLink = ({isActive})=> (isActive? `${styles.active}`: "")
 
 const Navbar = () => {
+  const userName = useSelector(selectUserName)
   return (
-    <div>
-        <h1>Navbar</h1>
+  <div className={styles.navbar}>
+      <div className={styles.user}>
+        <FaUserCircle size={40} color="#fff"/>
+        <h4>{userName}</h4>
+      </div>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/admin/home" className=
+            {activeLink}>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/admin/all-products" className=
+            {activeLink}>
+            All Products
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/admin/add-product" className=
+            {activeLink}>
+            Add Product
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/admin/orders" className=
+            {activeLink}>
+            Orders
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
     </div>
   );
 };
