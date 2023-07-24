c3bbfb694542a7c80ae12d31bb6f044f396e6884
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-07-25 02:14:10 +0800

    Add Validation in Signup

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -8,6 +8,7 @@ import {createUserWithEmailAndPassword} from 'firebase/auth'
 import { auth } from "../../firebase/config"
 import Loader from "../../components/loader/Loader"
 import { toast, ToastContainer } from 'react-toastify'
+import { BsEye, BsEyeSlash, BsFillCheckCircleFill, BsFillExclamationCircleFill } from "react-icons/bs"
 
 const SignUp = () => {
   const [email, setEmail] = useState("")
@@ -39,8 +40,100 @@ const registerUser = (e) =>{
           
       });
     }
+  
+    // let lowerCase = document.getElementById('lower');
+    // let upperCase = document.getElementById('upper');
+    // let digit = document.getElementById('number');
+    // let specialChar = document.getElementById('special');
+    // let minLength= document.getElementById('length');
 
+    // function checkPassword(data) {
+    //   const lower = new RegExp('(?=.*[a-z])');
+    //   const upper = new RegExp('(?=.*[A-Z])')
+    //   const number = new RegExp('(?=.*[0-9])')
+    //   const special = new RegExp('(?=.*[!@#\$%\^&\*])')
+    //   const length= new RegExp('(?=.{8})')
+
+    //   //Lowercase Validation check
+    //   if(lower.test(data)){
+    //     lowerCase.classList.add("valid");
+    //   }else{
+    //     lowerCase.classList.remove("valid");
+    //   }
+    //   //Uppercase Validation check
+    //   if(upper.test(data)){
+    //     upperCase.classList.add("valid");
+    //   }else{
+    //     upperCase.classList.remove("valid");
+    //   }
+    //   //Number Validation check
+    //   if(number.test(data)){
+    //     digit.classList.add("valid");
+    //   }else{
+    //     digit.classList.remove("valid");
+    //   }
+    //   //Special Charac Validation check
+    //   if(special.test(data)){
+    //     specialChar.classList.add("valid");
+    //   }else{
+    //     specialChar.classList.remove("valid");
+    //   }
+    //   //Length Validation check
+    //   if(length.test(data)){
+    //     minLength.classList.add("valid");
+    //   }else{
+    //     minLength.classList.remove("valid");
+    //   }
+    // }
+    //Validation states
+    const [visible , setVisible] = useState(false);
+    const [lowerValidated, setLowerValidated] = useState(false);
+    const [upperValidated, setUpperValidated] = useState(false);
+    const [numberValidated, setNumberValidated] = useState(false);
+    const [specialValidated, setSpecialValidated] = useState(false);
+    const [lengthValidated, setLengthValidated] = useState(false);
+
+    const handleChange=(value) =>{
+      const lower = new RegExp('(?=.*[a-z])');
+      const upper = new RegExp('(?=.*[A-Z])')
+      const number = new RegExp('(?=.*[0-9])')
+      const special = new RegExp('(?=.*[!@#\$%\^&\*])')
+      const length= new RegExp('(?=.{8})')
+      //Lowercase Validation check
+      if(lower.test(value)){
+        setLowerValidated(true);
+      }else{
+        setLowerValidated(false);
+      }
+      //Uppercase Validation check
+      if(upper.test(value)){
+        setUpperValidated(true);
+      }else{
+        setUpperValidated(false);
+      }
+      //Number Validation check
+      if(number.test(value)){
+        setNumberValidated(true);
+      }else{
+        setNumberValidated(false);
+      }
+      //Special Charac Validation check
+      if(special.test(value)){
+        setSpecialValidated(true);
+      }else{
+        setSpecialValidated(false);
+      }
+      //Length Validation check
+      if(length.test(value)){
+        setLengthValidated(true);
+      }else{
+        setLengthValidated(false);
+      }
+    }
+      
+  
   return (
+    
     <>
     <ToastContainer position='top-left'
             autoClose= {2000}
@@ -61,8 +154,19 @@ const registerUser = (e) =>{
           
           <form onSubmit={registerUser}>
               <input type='text' placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)}  />
-              <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}  required />
-              <input type='password' placeholder='Confirm Password' value={cPassword} onChange={(e) => setCPassword(e.target.value)} required />
+              <div className={styles['password-box']}>
+              <input type={visible ? "text" : "password"} placeholder='Password' value={password} onChange={(e) => {setPassword(e.target.value); handleChange(e.target.value)}}  required />
+              <div onClick={()=> setVisible(!visible)} className={styles.eyePassword}>
+                  {visible ? <BsEye/> : <BsEyeSlash/>}
+                </div>
+              </div>
+
+              <div className={styles['password-box']}>
+              <input type={visible ? "text" : "password"} placeholder='Confirm Password' value={cPassword} onChange={(e) => setCPassword(e.target.value)} required />
+              <div onClick={()=> setVisible(!visible)} className={styles.eyePassword}>
+                  {visible ? <BsEye/> : <BsEyeSlash/>}
+                </div>
+              </div>
               <button type="submit" className='--btn --btn-primary --btn-block'>Sign Up</button>
               
           </form>
@@ -70,13 +174,60 @@ const registerUser = (e) =>{
             <p>Already have an account? </p>
             <Link to='/login'>Login</Link>
           </span>
+          <div className={styles["tracker-box"]}>
+            <div className={lowerValidated? styles.validated: "not-validated"}>{lowerValidated?(
+              <span className={styles["list-icon"]}>
+                <BsFillCheckCircleFill/>
+              </span>
+            ):(
+              <span className={styles["list-icon"]}>
+                <BsFillExclamationCircleFill/>
+              </span>
+            )}At least one lowercase character</div>
+            <div className={upperValidated? styles.validated: "not-validated"}>{upperValidated?(
+              <span className={styles["list-icon"]}>
+                <BsFillCheckCircleFill/>
+              </span>
+            ):(
+              <span className={styles["list-icon"]}>
+                <BsFillExclamationCircleFill/>
+              </span>
+            )}At least one uppercase character</div>
+            <div className={numberValidated? styles.validated: "not-validated"}>{numberValidated?(
+              <span className={styles["list-icon"]}>
+                <BsFillCheckCircleFill/>
+              </span>
+            ):(
+              <span className={styles["list-icon"]}>
+                <BsFillExclamationCircleFill/>
+              </span>
+            )}At least one number</div>
+            <div className={specialValidated? styles.validated: "not-validated"}>{specialValidated?(
+              <span className={styles["list-icon"]}>
+                <BsFillCheckCircleFill/>
+              </span>
+            ):(
+              <span className={styles["list-icon"]}>
+                <BsFillExclamationCircleFill/>
+              </span>
+            )}At least one special character</div>
+            <div className={lengthValidated? styles.validated: "not-validated"}>{lengthValidated?(
+              <span className={styles["list-icon"]}>
+                <BsFillCheckCircleFill/>
+              </span>
+            ):(
+              <span className={styles["list-icon"]}>
+                <BsFillExclamationCircleFill/>
+              </span>
+            )}At least 8 characters</div>
+          </div>
           
         </div>
       </Card>
       <div className={styles.img}>
-        <img src= {signupBg} alt='Login' width="220px"/>
+        <img src= {signupBg} alt='Login' width="330px"/>
       </div>
-
+      
     </section>
     </>
   )
