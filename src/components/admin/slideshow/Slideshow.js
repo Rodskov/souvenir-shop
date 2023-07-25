f6586a312106b5f4d63860e328156bf2f8159e35
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2023-07-26 00:38:32 +0800

    Better slideshow editor

--- src/components/admin/slideshow/Slideshow.js
+++ src/components/admin/slideshow/Slideshow.js
@@ -3,6 +3,7 @@ import styles from "./Slideshow.module.scss"
 import Card from '../../card/Card'
 import { getStorage, uploadBytes, ref, getDownloadURL } from 'firebase/storage';
 import { doc, getDoc, getFirestore, setDoc, snapshotEqual } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const Slideshow = () => {
   const [imageLinksArray, setImageLinksArray] = useState([]);
@@ -11,14 +12,18 @@ const Slideshow = () => {
   const [confirmation, setConfirmation] = useState(false)
   const [btnDisplay, setBtnDisplay] = useState("none")
   const [confirmBtnDisplay, setConfirmBtnDisplay] = useState("none")
-  
-  const [imageDisplay, setImageDisplay] = useState("inline")
-  const [imageFromDatabase, setImageFromDatabase] = useState([])
   const [confirmText, setConfirmText] = useState("none")
+  const [imageDisplay, setImageDisplay] = useState("inline")
+  const [statsDisplay, setStatsDisplay] = useState("none")
 
+  const [imageFromDatabase, setImageFromDatabase] = useState([])
+  const [numFiles, setNumFiles] = useState([])
+  
   const [authentication, setAuthentication] = useState(0)
   const [authenticator, setAuthenticator] = useState(0)
 
+
+  const [status, setStatus] = useState(0)
   const storage = getStorage()
   const database = getFirestore()
   const collectionRef = doc(database, "slideshow", "slideshow")
@@ -30,6 +35,10 @@ const Slideshow = () => {
       setImageFromDatabase(snapshot.data().data)
     })
   }, [changeDisplay])
+
+  const statusText = {
+    display: statsDisplay
+  }
 
   const textConfirmation = {
     display: confirmText,
@@ -47,6 +56,13 @@ const Slideshow = () => {
     display: imageDisplay,
     height: "108px",
     width: "192px"
+  }
+
+  const cancel = () => {
+    setBtnDisplay("none")
+    setConfirmBtnDisplay("none")
+    setConfirmText("none")
+    setImageDisplay("none")
   }
 
   // Create a handler for submission
@@ -68,8 +84,9 @@ const Slideshow = () => {
       setChangeDiplay(Math.random())
       setImageDisplay("none")
       setConfirmBtnDisplay("none")
+      setStatsDisplay("none")
       setConfirmText("none")
-      alert("Data succesfully uploaded")
+      toast.success("Slideshow successfully changed!")
     })
   }
 
@@ -109,12 +126,16 @@ const Slideshow = () => {
   const handleEventChange = async (event) => {
     console.log(event.target.files)
     if(event.target.files.length != 0){
+      var statusUpdate = 0;
+      setStatsDisplay("block")
+      setStatus(statusUpdate)
       setConfirmBtnDisplay("none")
       setBtnDisplay("block")
       setImageLinksArray([])
       setImageDownloadArray([])
       const imageLinks = [];
       var authenticatorChild = 0;
+      setNumFiles(event.target.files.length)
       const authenticationChild = event.target.files.length
       setAuthentication(authenticatorChild)
       setAuthenticator(authenticationChild)
@@ -128,6 +149,8 @@ const Slideshow = () => {
         const imageRef = ref(storage, "slideshows/"+fileName)
         console.log(fileName)
         await uploadBytes(imageRef, receiveFiles[i]).then((snapshot) => {
+          statusUpdate++
+          setStatus(statusUpdate)
           authenticatorChild++
           console.log(authenticatorChild)
           console.log(authenticationChild)
@@ -139,6 +162,9 @@ const Slideshow = () => {
       setImageLinksArray(imageLinks)
     }
     else{
+      setConfirmText("none")
+      setImageDisplay("none")
+      setStatsDisplay("none")
       setConfirmBtnDisplay("none")
       setBtnDisplay("none")
     }
@@ -156,13 +182,17 @@ const Slideshow = () => {
             </label>
             <Card cardClass={styles.group}>
             <div>
-              <input onChange={handleEventChange}id="imageFiles" type="file" multiple/>
+              <input onChange={handleEventChange} id="imageFiles" type="file" multiple/>
             </div>
             <div>
               <button style={confirmButtonStyle}>Confirm New Slideshow</button>
+              <button type="button" style={confirmButtonStyle} onClick={cancel}>Cancel</button>
             </div>
             </Card>
           </form>
+          <div>
+            <p style={statusText}>{status}/{numFiles} file(s) uploaded</p>
+          </div>
           <div>
             <button onClick={confirmImages} style={buttonStyle}>Confirm Images</button>
           </div>
