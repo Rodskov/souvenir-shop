8f78cd7fb8d3ba4a351d7dc3ac20946c34fe94ec
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2023-07-29 22:59:01 +0800

    Slideshow Finish

--- src/pages/wishlist/Wishlist.js
+++ src/pages/wishlist/Wishlist.js
@@ -18,8 +18,12 @@ const Wishlist = () => {
     const [wishlistData, setWishlistData] = useState("")
     const [newWishlist, setNewWishlist] = useState("")
     const [imageArray, setImageArray] = useState([])
-    const [imageLinkArray, setImageLinkArray] = useState([])
-    const [imageDownloadArray, setImageDownloadArray] = useState([])
+    const [authenticator, setAuthenticator] = useState(false)
+
+    const [numOfFiles, setNumOfFiles] = useState(0)
+    const [uploadedFiles, setUploadedFiles] = useState(0)
+
+    const [uploadState, setUploadState] = useState("none")
 
     const isLoggedIn = useSelector(selectIsLoggedIn); 
     const userID = useSelector(selectUserID)
@@ -36,43 +40,82 @@ const Wishlist = () => {
     width: "192px"
     }
 
+    const textStyle = {
+      display: uploadState,
+    }
+
+    var imageLinks = [];
+    var imageDownloadArray = [];
+
+    useEffect(() => {
+      console.log("Updated imageArray:", imageArray);
+    }, [imageArray]);
+
+    useEffect(() => {
+      console.log("numOfFiles updated:", numOfFiles);
+      // Perform any actions you need after numOfFiles is updated
+    }, [numOfFiles]);
+
+    useEffect(() => {
+      console.log(authenticator)
+    }, [authenticator])
+
+    useEffect(() => {
+      console.log(numOfFiles)
+    }, [numOfFiles])
+    
     const linkFetcher = async () => {
       console.log("LinkFetcher called")
       try{
-        const urls = await Promise.all(imageLinkArray.map((URLs) => {
+        const urls = await Promise.all(imageLinks.map((URLs) => {
           const imageRef = ref(storage, URLs);
-          return getDownloadURL (imageRef);
+          return getDownloadURL(imageRef);
         })
         );
-          setImageDownloadArray(urls)
+          imageDownloadArray = urls
           console.log(imageDownloadArray)
+          setImageArray(urls)
+          console.log(imageArray)
       } catch(error) {
         console.error("error", error)
       }
     }
 
     const fileReceiver = async (e) => {
-      console.log(e.target.files);
-      const arrayImage = e.target.files
-      setImageLinkArray([])
-      setImageDownloadArray([])
-      console.log(typeof arrayImage)
-      console.log(arrayImage)
-      const imageLinks = []
-      setImageArray(e.target.files);
-      for(var i=0; i < arrayImage.length; i++){
-        const fileName = arrayImage[i].name
-        console.log(fileName)
-        imageLinks.push("gs://pup-souvenir-shop.appspot.com/wishlist/"+fileName)
-        const imageRef = ref(storage, "wishlist/"+fileName)
-        await uploadBytes(imageRef, arrayImage[i]).then((snapshot) => {
-          console.log(fileName+" uploaded")
-        })
+      if(e.target.files.length != 0){
+        setUploadState("block")
+        setNumOfFiles(e.target.files.length)
+        console.log(e.target.files);
+        const arrayImage = e.target.files
+        console.log(typeof arrayImage)
+        console.log(arrayImage)
+        var uploaded = 0
+        imageDownloadArray = []
+        imageLinks = []
+        for(var i=0; i < arrayImage.length; i++){
+          const fileName = arrayImage[i].name
+          console.log(fileName)
+          imageLinks.push("gs://pup-tanglaw-clothing.appspot.com/wishlist/"+fileName)
+          const imageRef = ref(storage, "wishlist/"+fileName)
+          await uploadBytes(imageRef, arrayImage[i]).then((snapshot) => {
+            uploaded++
+            console.log(numOfFiles)
+            console.log(uploaded)
+            console.log(uploaded+"/"+numOfFiles+": uploaded")
+          })
+        }
+          setUploadedFiles(uploaded)
+          console.log(uploadedFiles)
+          console.log(numOfFiles)
+          console.log(imageLinks)
+          setAuthenticator(true)
+          linkFetcher()
       }
-        setImageLinkArray(imageLinks)
-        console.log(imageLinks)
-        console.log(imageLinkArray)
-        linkFetcher()
+      else{
+        setNumOfFiles(e.target.files.length)
+        setAuthenticator(false)
+        setUploadState("none")
+      }
     };
 
     
@@ -80,30 +123,67 @@ const Wishlist = () => {
   
     const addWishlist = (e) =>{
       e.preventDefault()
-      const today = new Date();
-      const date =  today.toDateString();
+      if(authenticator != false){
+        const today = new Date();
+        const date =  today.toDateString();
+    
+        const wishlistConfig = {
+          userID,
+          userName,
+          wishlist: newWishlist,
+          wishlistDate: date,
+          images: imageArray,
+          createdAt: Timestamp.now().toDate()
+        }
+        if (!isLoggedIn) {
+          toast.error("Please log in to make a wishlist.");
+          return;
+      }
   
-      const wishlistConfig = {
-        userID,
-        userName,
-        wishlist: newWishlist,
-        wishlistDate: date,
-        images: "",
-        createdAt: Timestamp.now().toDate()
+        try {
+          addDoc(collection(db, "wishlist"), wishlistConfig);
+          toast.success("Wishlist Submitted");
+          setNewWishlist("")
+        } catch(error) {
+          toast.error(error.message);
+        }
+        //setEventChanger(Math.random())
+        setTimeout(() => {
+          window.location.reload()
+        }, 1000 )
       }
-      if (!isLoggedIn) {
-        toast.error("Please log in to make a wishlist.");
-        return;
-    }
-
-      try {
-        addDoc(collection(db, "wishlist"), wishlistConfig);
-        toast.success("Wishlist Submitted");
-        setNewWishlist("")
-      } catch(error) {
-        toast.error(error.message);
+      else if (numOfFiles === 0){
+        const today = new Date();
+        const date =  today.toDateString();
+    
+        const wishlistConfig = {
+          userID,
+          userName,
+          wishlist: newWishlist,
+          wishlistDate: date,
+          images: imageArray,
+          createdAt: Timestamp.now().toDate()
+        }
+        if (!isLoggedIn) {
+          toast.error("Please log in to make a wishlist.");
+          return;
       }
-      setEventChanger(Math.random())
+  
+        try {
+          addDoc(collection(db, "wishlist"), wishlistConfig);
+          toast.success("Wishlist Submitted");
+          setNewWishlist("")
+        } catch(error) {
+          toast.error(error.message);
+        }
+        //setEventChanger(Math.random())
+        setTimeout(() => {
+          window.location.reload()
+        }, 1000 )
+      }
+      else{
+        toast.error("Images are still uploading.")
+      }
     }
     
     useEffect(() => {
@@ -111,6 +191,7 @@ const Wishlist = () => {
           try {
             const querySnapshot = await getDocs(collection(db, "wishlist"));
             const wishlistData = querySnapshot.docs.map((doc) => doc.data());
+            console.log(wishlistData)
             setWishlistData(wishlistData);
             setLoading(false);
           } catch (error) {
@@ -119,7 +200,7 @@ const Wishlist = () => {
           }
         };
         fetchWishlist();
-      }, [eventChanger]);
+      }, []);
            
   return (
     <section>
@@ -137,11 +218,7 @@ const Wishlist = () => {
           <h5>Choose your image/s:</h5>
           <input type="file" onChange={fileReceiver} multiple/>
           <div>
-            {imageDownloadArray.map((data, i) => {
-              return(
-                <img style={imageStyle} src={data}></img>
-              )
-            })}
+            <p style={textStyle}>{uploadedFiles}/{numOfFiles} Uploaded</p>
           </div>
           <button type='submit' className='--btn --btn-primary3'>
             Submit Wishlist
@@ -161,6 +238,13 @@ const Wishlist = () => {
                 <p><b>Username:</b> {item.userName}</p>
                 <p><b>Date:</b> {item.wishlistDate}</p>
                 <p> {item.wishlist}</p>
+                <Card>
+                  {item.images.map((imageSource, i) => {
+                    return(
+                      <img key={i} style={imageStyle} src={imageSource}/>
+                    )
+                  })}
+                </Card>
               </div>
             </div>
              
