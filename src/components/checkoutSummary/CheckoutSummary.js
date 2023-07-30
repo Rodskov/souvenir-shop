19f53f237d32f309a253d7a06fee3b2288fcc7a0
Kyle Carandang <92615736+kylekrc@users.noreply.github.com>
2023-07-31 03:20:00 +0800

    CheckoutDetails Design

--- src/components/checkoutSummary/CheckoutSummary.js
+++ src/components/checkoutSummary/CheckoutSummary.js
@@ -8,6 +8,7 @@ import styles from "./CheckoutSummary.module.scss"
 import { toast } from "react-toastify";
 import { shippingFeeAmount } from "../../redux/slice/checkoutSlice";
 
+
 const CheckoutSummary = ( ) => {
     const cartItems = useSelector(selectCartItems);
     const shipFee = useSelector(shippingFeeAmount)
@@ -23,42 +24,47 @@ const CheckoutSummary = ( ) => {
     }, [shipFee])
     
     return <div>
-        <h3>Checkout Summary</h3>
-        <div>
-            {cartItems.length === 0 ? (
-                <>
-                <p>No item in your cart.</p>
-                <button className="--btn">
-                    <Link to="/#products">Back To Shop</Link>
-                </button>
-                </>
-            ) : (
-                <div>
-                    <p>
-                        <b>{`Cart item(s): ${cartTotalQuantity}`}</b>
-                    </p>
-                    <div className={styles.text}>
-                        <h4>Subtotal: </h4>
-                        <h3>{totalAmount.toFixed(2)}</h3>
-                        <div></div>
-                        <p>Shipping Fee: { shipFee }</p>
+        <h3 className={styles.title}>Checkout Summary</h3>
+            {cartItems.map((item, index) => {
+                const {id, name, price, cartQuantity, size, color } = item
+                return (
+                    <div key={id} className={styles.checkout_prodList}>
+                        <h4>Product: <b className={styles.title}>{name}</b></h4>
+                        <p><b>Variation: </b>{size}-{color}</p>
+                        <p><b>Quantity: </b>{cartQuantity}</p>
+                        <p><b>Unit Price: </b>{price}</p>
+                        <p><b>Set Price: </b>{price * cartQuantity}</p>
+                        
+                        {/* <p>Selected Province: {selectedProvince}</p> */}
                     </div>
-                    {cartItems.map((item, index) => {
-                        const {id, name, price, cartQuantity} = item
-                        return (
-                            <Card key={id} cardClass={styles.card}>
-                                <h4>Product: {name}</h4>
-                                <p>Quantity: {cartQuantity}</p>
-                                <p>Unit Price: {price}</p>
-                                <p>Set Price: {price * cartQuantity}</p>
-                                
-                                {/* <p>Selected Province: {selectedProvince}</p> */}
-                            </Card>
-                        )
-                    })}
-                </div>
-            )}
-        </div>
+                )
+            })}
+                
+            <Card cardClass={styles.card}>
+            <div>
+                {cartItems.length === 0 ? (
+                    <>
+                    <p>No item in your cart.</p>
+                    <button className="--btn">
+                        <Link to="/#products">Back To Shop</Link>
+                    </button>
+                    </>
+                ) : (
+                    <div className={styles.subtotal}>
+                        <p className={styles.ship}>Shipping Fee: { shipFee }</p>
+                        <p className={styles.cartItems}>
+                            <b>{`Cart item(s): ${cartTotalQuantity}`}</b>
+                        </p>
+                        <div className={styles.text}>
+                            <h4 className={styles.subtotal1}>Subtotal: </h4>
+                            <h3>{totalAmount.toFixed(2)}</h3>
+                            <div></div>
+                            
+                        </div>
+                    </div>
+                )}
+            </div>
+            </Card>
     </div>;
 };
 
