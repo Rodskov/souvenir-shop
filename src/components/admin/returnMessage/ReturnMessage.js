74e45163f06046a6fb76f38813f1ac98757e9fa5
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2023-07-31 16:20:18 +0800

    Revert "Images and Video on Return"
    This reverts commit 7e0bdbb7794f3fe4470bbc48d71b3f2a4b746ca6.

--- src/components/admin/returnMessage/ReturnMessage.js
+++ src/components/admin/returnMessage/ReturnMessage.js
@@ -83,29 +83,6 @@ const ReturnMessage = () => {
                   {returns.returns}
                   {returns.productID}
                   </p>
-                  {returns.videoURLs !=0 ? (
-                    returns.videoURLs.map((links, i) => {
-                      return(
-                        <div key={i}>
-                          <video src={links} style={imageStyleHardCoded} autoPlay controls/>
-                        </div>
-                      )
-                    })
-                  ) : (
-                    console.log("Return has no media files")
-                  )}
-                  {returns.imageURLs != 0 ? (
-                    returns.imageURLs.map((links, i) => {
-                      return (
-                        <div key={i} style={{display: 'inline'}}>
-                          <img src={links} style={imageStyleHardCoded}/>
-                        </div>
-                      )
-                    })
-                  ) : (
-                    console.log("Return has no media files")
-                  )
-                  }
                 </Card>
               ) : (
                 <p>Loading...</p>
